75d763017d11fbaed8463de4b59865df7b8116f8
thinhvt03 <nguyenducthinhvt03@gmail.comgit config --global user.email nguyenducthinhvt03@gmail.com>
2023-03-25 16:05:04 +0700

    update load Image using Kingfisher

--- TableViewGuide_RESTAPI/ViewController.swift
+++ TableViewGuide_RESTAPI/ViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
@@ -16,10 +17,38 @@ class ViewController: UIViewController, UITableViewDataSource {
             }
         }, error: nil)
     }
+    
+    private func downloadImage(imageView: UIImageView, url: URL) {
+        imageView.kf.indicatorType = .activity
+        imageView.kf.setImage(with: url,
+                              placeholder: UIImage(named: "placeholderImage"),
+                              options: [
+                                .processor(DownsamplingImageProcessor(size: imageView.bounds.size)),
+                                .scaleFactor(UIScreen.main.scale),
+                                .cacheOriginalImage,
+                                .transition(.fade(0.25))
+                              ])
+        {
+            result in
+            switch result {
+            case .success(let value):
+                imageView.image = value.image
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
+    }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "StoryCell") as! StoryCell
         cell.story = stories[indexPath.row]
+        var urlImage: [String] = []
+        for story in stories {
+            urlImage.append(story.thumbnailUrl!)
+        }
+        if let urlString = URL(string: urlImage[indexPath.row]) {
+            downloadImage(imageView: cell.thumbnailView, url: urlString)
+        }
         return cell
     }
 
